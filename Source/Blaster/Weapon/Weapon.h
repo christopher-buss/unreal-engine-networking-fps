edc4cab8281798d9b134ecfd1c2d37bba298530e
christopher-buss <christopher.buss+github@protonmail.com>
2024-02-21 00:36:28 +0000

    feat: add weapon class

---
+++ Source/Blaster/Weapon/Weapon.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+class USphereComponent;
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	Initial UMETA(DisplayName = "Initial"),
+	Equipped UMETA(DisplayName = "Equipped"),
+	Dropped UMETA(DisplayName = "Dropped"),
+	Max UMETA(DisplayName = "Max")
+};
+
+UCLASS()
+class BLASTER_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "WeaponProperties", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "WeaponProperties", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USphereComponent> AreaSphere;
+
+	UPROPERTY(VisibleAnywhere)
+	EWeaponState WeaponState;
+};
